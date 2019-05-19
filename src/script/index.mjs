0e29520a7c728fab8d9c88f1f987adba144def1a
Oliver Turner <oliverturner@users.noreply.github.com>
2019-05-19 13:01:48 +0100

    mobilenav (#11)
    * Add page describing hosting requirements
    
    * Add email link
    
    * Add responsive nav
    
    * Fix stacking
    
    * Remove motion preferences
    
    * Refine menu button
    
    * Revert change to colours
    
    * Fix z-indexing issues blocking footer links
    
    * Update deps
    
    * Switch to appearance for styling buttons
    
    * Add Twitter card
    
    * Fix absolute links to assets

--- src/script/index.mjs
+++ src/script/index.mjs
@@ -12,3 +12,12 @@ class SuperMannerist extends HTMLElement {
 
 // Define the new element
 customElements.define("super-mannerist", SuperMannerist);
+
+// Instantiate menu
+const $ = document.querySelector.bind(document);
+const btn = $("[data-click=menu-toggle]");
+const body = $("body");
+
+btn.addEventListener("click", () => {
+  body.classList.toggle("menu-closed");
+});
