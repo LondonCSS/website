bea1b75c90d7d61c762a2d98c8cecfb6413509b5
Oliver Turner <oliverturner@users.noreply.github.com>
2019-06-02 15:37:33 +0200

    a11y and assorted issues (#13)
    * Fix aria visibility on icons
    
    * Add lighthouse budgeting
    
    * Update deps
    
    * Add icons and webmanifest
    
    * Export all contents of /static

--- .eleventy.js
+++ .eleventy.js
@@ -29,7 +29,7 @@ const eleventy = config => {
   });
 
   config.addPassthroughCopy("src/script");
-  config.addPassthroughCopy("static/images");
+  config.addPassthroughCopy("static");
 
   return {
     dir: {
