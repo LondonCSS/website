f269c229909f40da3e9e51ea4aa99ea5d827c35a
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-06-05 11:19:45 +0100

    Modularise footer links

--- tools/postcss.config.js
+++ tools/postcss.config.js
@@ -1,14 +1,14 @@
-// const postcssImport = require("postcss-import");
 const postcssPseudoEnter = require("postcss-pseudo-class-enter");
 const postcssPresetEnv = require(`postcss-preset-env`);
 const postcssClean = require(`postcss-clean`);
 const postcssInset = require(`postcss-inset`);
+const postcssImport = require("postcss-import");
 
 const { customMedia } = require("../src/theme.js");
 
 module.exports = () => ({
-  from: "src/_includes/css",
   plugins: [
+    postcssImport(),
     postcssPresetEnv({
       preserve: true,
       browsers: "last 2 versions",
