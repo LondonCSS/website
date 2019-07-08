b2301db8b33bda17a22bdbf7338db5223d60293b
Oliver Turner <oliverturner@users.noreply.github.com>
2019-07-08 10:03:00 +0100

    Event 2 (#15)
    * Modularise footer links
    
    * Add events page and Michelle Barker talk

---
+++ src/_data/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  socialmedia: [
+    { 
+      href: "mailto:hello@londoncss", 
+      icon: "email", 
+      label: "Email" 
+    },
+    { 
+      href: "https://twitter.com/londoncss", 
+      icon: "twitter", 
+      label: "Follow" 
+    },
+    {
+      href: "https://github.com/LondonCSS/website",
+      icon: "github",
+      label: "Source"
+    }
+  ]
+};
