8fe41d101dcef526983c25e1a26885067c0eb7b6
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-07-28 20:53:43 +0100

    Improve naming downloaded files

--- tools/wallpapers.js
+++ tools/wallpapers.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 const puppeteer = require("puppeteer");
 const sharp = require("sharp");
-const slugify = require("slugify");
 
 const mkdirp = util.promisify(require("mkdirp"));
 const readFile = util.promisify(fs.readFile);
@@ -12,21 +11,20 @@ const readFile = util.promisify(fs.readFile);
 const { wallpapers } = require("../src/_data/config");
 const savePath = path.join(__dirname, "../dist/static/wallpapers");
 
-async function screenshotPage(browser, id, title) {
-  const slug = slugify(title);
+async function screenshotPage(browser, id) {
   const page = await browser.newPage();
   await page.goto(`https://s.codepen.io/oliverturner/debug/${id}`, {
     waitUntil: "networkidle2"
   });
 
   await page.setViewport({ width: 3840, height: 2160, deviceScaleFactor: 2 });
-  await page.screenshot({ path: `${savePath}/${slug}-cinema.png` });
+  await page.screenshot({ path: `${savePath}/${id}-cinema.png` });
 
   await page.setViewport({ width: 1440, height: 900, deviceScaleFactor: 2 });
-  await page.screenshot({ path: `${savePath}/${slug}-desktop.png` });
+  await page.screenshot({ path: `${savePath}/${id}-desktop.png` });
 
   await page.setViewport({ width: 414, height: 744, deviceScaleFactor: 2 });
-  await page.screenshot({ path: `${savePath}/${slug}-mobile.png` });
+  await page.screenshot({ path: `${savePath}/${id}-mobile.png` });
 }
 
 async function thumbnailImage(id) {
