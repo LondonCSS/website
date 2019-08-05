cf0fa7a3a698b056740c56c3f0fa014fc76b2d6e
Oliver Turner <oliver.turner@codedsignal.co.uk>
2019-08-05 20:57:57 +0100

    Add Una's extra-confetti

--- src/js/puppeteer.js
+++ src/js/puppeteer.js
@@ -26,8 +26,69 @@ CSS.registerProperty({
   initialValue: "multiply"
 });
 
+CSS.registerProperty({
+  name: '--extra-sparkleNumber',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 30,
+});
+
+CSS.registerProperty({
+  name: '--extra-sparkleHue',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 60,
+});
+
+// Sparkles
+//------------------------------------------------------------------------------
+CSS.registerProperty({
+  name: '--extra-sparkleHeightVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 9,
+});
+
+CSS.registerProperty({
+  name: '--extra-sparkleWidthVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 12,
+});
+
+CSS.registerProperty({
+  name: '--extra-sparkleWeightVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 2,
+});
+
+// Confetti
+//------------------------------------------------------------------------------
+CSS.registerProperty({
+  name: '--extra-confettiNumber',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 30,
+});
+
+CSS.registerProperty({
+  name: '--extra-confettiLengthVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 15,
+});
+
+CSS.registerProperty({
+  name: '--extra-confettiWeightVariance',
+  syntax: '<number>',
+  inherits: false,
+  initialValue: 4,
+});
+
 async function init() {
   try {
+    CSS.paintWorklet.addModule("/static/paintlets/extra.css/confetti.js");
     CSS.paintWorklet.addModule("/static/paintlets/malchata/circles.js");
   } catch (err) {
     console.log(err);
