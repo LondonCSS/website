d6a4a8738d02bf5f27ed4bb251c28f5e99d96aa1
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-03-12 13:30:19 +0000

    Unregister SW

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,20 @@ btn.addEventListener("click", () => {
 
 // Load Service Worker
 //------------------------------------------------------------------------------
+/*
 const isProd = location.hostname !== "localhost";
+
 if (isProd && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
+  });
+}
+*/
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
   });
 }
