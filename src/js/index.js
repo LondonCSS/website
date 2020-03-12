0122a78c965b8cd32edc53612b668af1c6d9ce69
Oliver Turner <oliverturner@users.noreply.github.com>
2020-03-12 15:11:54 +0000

    Sw unregister (#50)
    * Unregister SW
    
    * Unregister SW

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,20 @@ btn.addEventListener("click", () => {
 
 // Load Service Worker
 //------------------------------------------------------------------------------
+/*
 const isProd = location.hostname !== "localhost";
+
 if (isProd && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
+  });
+}
+*/
+
+if (window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
   });
 }
