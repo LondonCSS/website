a61b69cdd242aa09037cc836b40b9353b0188585
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-09-05 09:34:45 +0100

    Stub out working Sass pipeline

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,28 @@
 const pluginTOC = require("eleventy-plugin-toc");
-
 const MarkdownIt = require("markdown-it");
 const { format: formatDate, parseISO } = require("date-fns");
+
+const { renderSass } = require("./config/sass");
 
 const now = new Date();
 
 const md = new MarkdownIt({
   xhtmlOut: true,
-  typographer: true
+  typographer: true,
 }).use(require("markdown-it-anchor"));
 
-const eleventy = config => {
+const eleventy = (config) => {
   config.addPlugin(pluginTOC);
 
-  config.addFilter("date", code => formatDate(parseISO(code), "EEEE, MMMM do yyyy"));
+  config.addFilter("date", (code) =>
+    formatDate(parseISO(code), "EEEE, MMMM do yyyy")
+  );
 
-  config.addFilter("markdown", code => {
+  config.addFilter("markdown", (code) => {
     return code ? md.render(code) : code;
   });
 
-  config.addFilter("isFuture", code => {
+  config.addFilter("isFuture", (code) => {
     return new Date(code) > now;
   });
 
@@ -28,16 +31,22 @@ const eleventy = config => {
   config.addPassthroughCopy("src/js");
   config.addPassthroughCopy("static");
 
+  config.addWatchTarget("./src/scss/");
+  config.addWatchTarget("./src/js/");
+
+  renderSass("src/scss/puppeteer.scss", "./dist/assets/puppeteer.css");
+  renderSass("src/scss/styles.scss", "./dist/assets/styles.css");
+
   return {
     dir: {
       input: "src",
       output: "dist",
-      includes: "_includes"
+      includes: "_includes",
     },
     passthroughFileCopy: true,
     templateFormats: ["njk", "md", "11ty.js"],
     htmlTemplate: "njk",
-    markdownTemplateEngine: "njk"
+    markdownTemplateEngine: "njk",
   };
 };
 
