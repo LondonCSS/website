c9e6c66fb08ba2261cd7367f1f8cf698efb749d7
Oliver Turner <oliver.turner@codedsignal.co.uk>
2020-09-05 10:24:48 +0100

    Add postcss compile step

--- tools/postcss.config.js
+++ tools/postcss.config.js
@@ -2,23 +2,21 @@ const postcssPseudoEnter = require("postcss-pseudo-class-enter");
 const postcssPresetEnv = require(`postcss-preset-env`);
 const postcssClean = require(`postcss-clean`);
 const postcssInset = require(`postcss-inset`);
-const postcssImport = require("postcss-import");
+// const postcssImport = require("postcss-import");
 
 const { customMedia } = require("../src/theme.js");
 
-module.exports = () => ({
-  plugins: [
-    postcssImport(),
-    postcssPresetEnv({
-      preserve: true,
-      browsers: "last 2 versions",
-      features: {
-        "nesting-rules": true,
-        "custom-media-queries": { importFrom: { customMedia } }
-      }
-    }),
-    postcssInset(),
-    postcssPseudoEnter(),
-    postcssClean()
-  ]
-});
+module.exports = () => [
+  // postcssImport(),
+  postcssPresetEnv({
+    preserve: true,
+    browsers: "last 2 versions",
+    features: {
+      "nesting-rules": true,
+      "custom-media-queries": { importFrom: { customMedia } },
+    },
+  }),
+  postcssInset(),
+  postcssPseudoEnter(),
+  postcssClean(),
+];
