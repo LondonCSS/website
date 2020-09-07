aa3b475ec8c9c181889bbc7fa0f238b3633cb3ab
Oliver Turner <oliverturner@users.noreply.github.com>
2020-09-07 09:34:24 +0100

    Feature/scss dynamic path (#70)
    * Refine transpilation
    
    * Use file stem location under ./src to determine output under ./dist
    
    * Remove extraneous deps

--- tools/postcss.config.js
+++ tools/postcss.config.js
@@ -1,20 +1,26 @@
 const postcssPseudoEnter = require("postcss-pseudo-class-enter");
 const postcssPresetEnv = require(`postcss-preset-env`);
 const postcssInset = require(`postcss-inset`);
-const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
 
 const { customMedia } = require("../src/theme.js");
 
-module.exports = () => [
-  postcssImport(),
-  postcssPresetEnv({
-    features: {
-      "custom-media-queries": { importFrom: { customMedia } },
-    },
-  }),
-  postcssInset(),
-  postcssPseudoEnter(),
-  cssnano({ preset: "advanced" }),
-]
+function getPlugins(isProd) {
+  const plugins = [
+    postcssPresetEnv({
+      features: {
+        "custom-media-queries": { importFrom: { customMedia } },
+      },
+    }),
+    postcssInset(),
+    postcssPseudoEnter(),
+  ];
 
+  if (isProd) {
+    return plugins.concat([cssnano({ preset: "advanced" })]);
+  }
+
+  return plugins;
+}
+
+module.exports = getPlugins(process.env.NODE_ENV === "production");
